4338bfed0f2643c152afcc41b1b276661f25a194
Mark <mark42son@gmail.com>
2025-04-21 15:30:43 +0900

    add: logger additional function - Info message, Error message

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -16,6 +16,22 @@ func NewLogger(filePath string) (*Logger, error) {
 	if err != nil {
 		return nil, err
 	}
-	log.SetOutput(file) // log 패키지의 출력 대상을 파일로 설정
+	log.SetOutput(file)                 // log 패키지의 출력 대상을 파일로 설정
+	log.SetFlags(log.Ldate | log.Ltime) // 로그에 날짜, 시간, 파일명 포함.
+	log.Println("Logger initialized.")  // 로그 초기화 메시지
 	return &Logger{file: file}, nil
+}
+
+func (l *Logger) Close() {
+	if l.file != nil {
+		l.file.Close() // 파일 닫기
+	}
+}
+
+func (l *Logger) Info(msg string) {
+	log.Println("INFO:", msg)
+}
+
+func (l *Logger) Error(msg string) {
+	log.Println("ERROR:", msg)
 }
