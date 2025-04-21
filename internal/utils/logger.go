4d7d096ba18eb1274fe27a70c7d1bc0f163aa5ae
Mark <mark42son@gmail.com>
2025-04-21 15:00:18 +0900

    feat : logger func : NewLogger - create log feature logic

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -7,12 +7,15 @@ import (
 	"os"
 )
 
-type Loger struct {
+type Logger struct {
 	file *os.File
 }
 
-func NewLogger(filePath string) {
-	pass
+func NewLogger(filePath string) (*Logger, error) {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+	log.SetOutput(file) // log 패키지의 출력 대상을 파일로 설정
+	return &Logger{file: file}, nil
 }
-
-
