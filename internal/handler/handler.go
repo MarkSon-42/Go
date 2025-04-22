a8d605407ce16cd75f0d907d722bc43b6cb11844
minwoo <mark42son@gmail.com>
2025-04-22 20:11:30 +0900

    feat : add client handler (not completed)

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,3 +1,30 @@
 package handler
 
+import (
+	"log"
+	"net"
+)
 
+// ClientHandler로 클라이언트 연결을 처리하는 구조체 선언
+type Handler struct {
+	Logger *log.Logger
+}
+
+func NewHandler(LOGGER *log.Logger) *Handler {
+	return &Handler{
+		Logger: LOGGER,
+	}
+}
+
+// 들어오는 클라이언트 연결을 처리하는 메서드
+func (h *Handler) HandleConnection(conn net.Conn) {
+	defer conn.Close() // 연결 종료 시 conn.Close() 호출
+
+	buffer := make([]byte, 1024)
+	for {
+		break
+		// 임시로 루프 종료 ( 기능 구현중..)
+	}
+
+	data := buffer[:n]
+}
